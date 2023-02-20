c0a3e8816fb4a3e262b07cbd3731fa904d4049c9
i-mak33-124 <i.124.takumi00@gmail.com>
2023-02-20 13:40:21 +0900

    forms,urls,views,settingとメモ

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,36 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin  # ログイン時のみ表示するようにする
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from .forms import SignupForm
+from .forms import LoginForm, SignupForm  # 指定の仕方が分からない
 
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = reverse_lazy("tweets:home")  # ?
 
     def form_valid(self, form):
-        response = super().form_valid(form)
+        response = super().form_valid(form)  # ?
         username = form.cleaned_data["username"]
         password = form.cleaned_data["password1"]
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class Login(LoginRequiredMixin, LoginView):  # 必ず先頭に
+    form_class = LoginForm  # Forms.pyのクラス(login,logoutはまだ未実装)
+    template_name = "accounts/login.html"  # 実際に表示されるファイル
+
+
+# form_classにform.pyで定義したLoginFormを指定することで、ログイン処理時にLoginFormで定義したフォームデザインが適用される。
+
+
+class Logout(LogoutView):  # form_classは不要
+    template_name = "accounts/login.html"  # そのままでいいのか？
+
+
+# 要件によるとuserprifileも追加する必要あり
